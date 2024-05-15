51e2ce96652675eccfb1d7d9608d1a72f733f550
Hugo Amuan <hugo.amuan@icloud.com>
2024-05-15 11:22:19 -0700

    Initialized server

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello, World!");
+const express = require('express');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+const app = express();
+
+// Could be useful in the future
+// require('dotenv').config();
+// const session = require('express-session');
+// const Joi = require("joi");
+// const path = require('path');
+// app.use(session({ 
+//     secret: node_session_secret,
+// 	store: mongoStore, //default is memory store 
+// 	saveUninitialized: false, 
+// 	resave: true
+// }
+// ));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
+
+
