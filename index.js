15acbbbf2758de0f4eb7e80357abbca6e604ddaf
Mitchell <mitchell.macdonald10@gmail.com>
2024-05-15 10:36:58 -0700

    .env and some user connection code

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+
+
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+
+
+var {database} = include('databaseConnection');
+
+const userCollection = database.db(mongodb_database).collection('users');
+
+var mongoStore = MongoStore.create({
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+	crypto: {
+		secret: mongodb_session_secret
+	}
+})
+
+app.use(session({ 
+    secret: node_session_secret,
+	store: mongoStore, 
+	saveUninitialized: false, 
+	resave: true
+}
+));
+
 console.log("Hello, World!");
