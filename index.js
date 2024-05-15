4fe096f065ef13e56f73c1ee5cee66569a63ca55
Mitchell <mitchell.macdonald10@gmail.com>
2024-05-15 11:35:37 -0700

    Init database branch

--- index.js
+++ index.js
@@ -2,18 +2,43 @@ const express = require('express');
 const fs = require('fs');
 const port = process.env.PORT || 3000;
 const app = express();
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+
+
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+
+
+var {database} = include('databaseConnection');
+
+const userCollection = database.db(mongodb_database).collection('users');
+
+var mongoStore = MongoStore.create({
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+	crypto: {
+		secret: mongodb_session_secret
+	}
+})
+
+app.use(session({ 
+    secret: node_session_secret,
+	store: mongoStore, 
+	saveUninitialized: false, 
+	resave: true
+}
+));
 
 // Could be useful in the future
 // require('dotenv').config();
 // const session = require('express-session');
 // const Joi = require("joi");
 // const path = require('path');
-// app.use(session({ 
-//     secret: node_session_secret,
-// 	store: mongoStore, //default is memory store 
-// 	saveUninitialized: false, 
-// 	resave: true
-// }
 // ));
 
 app.get('/', (req, res) => {
