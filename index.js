cf72b0b971e7acf9903214821cbb7f656f1a97e7
Gurshaan Daula <gurshaandaula@gmail.com>
2024-05-23 14:47:03 -0700

    Picture upload

--- index.js
+++ index.js
@@ -341,6 +341,43 @@ app.get('/orgDashboard', (req, res) => {
     res.render('orgDashboard');
 });
 
+import {v2 as cloudinary} from 'cloudinary';
+
+(async function() {
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: CLOUDINARY_CLOUD_NAME, 
+        api_key: CLOUDINARY_CLOUD_KEY, 
+        api_secret:CLOUDINARY_CLOUD_SECRET // Click 'View Credentials' below to copy your API secret
+    });
+    
+    // Upload an image
+    const uploadResult = await cloudinary.uploader.upload("https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg", {
+        public_id: "shoes"
+    }).catch((error)=>{console.log(error)});
+    
+    console.log(uploadResult);
+    
+    // Optimize delivery by resizing and applying auto-format and auto-quality
+    const optimizeUrl = cloudinary.url("shoes", {
+        fetch_format: 'auto',
+        quality: 'auto'
+    });
+    
+    console.log(optimizeUrl);
+    
+    // Transform the image: auto-crop to square aspect_ratio
+    const autoCropUrl = cloudinary.url("shoes", {
+        crop: 'auto',
+        gravity: 'auto',
+        width: 500,
+        height: 500,
+    });
+    
+    console.log(autoCropUrl);    
+})();
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
